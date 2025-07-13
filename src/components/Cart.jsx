56bdc5e1e40958f48aef5e3b40c9e3a6dfa69d6f
Andrew Kononenko <wastardy.k@gmail.com>
2025-07-13 07:37:20 +0300

    feat(component): implement logic to go to checkout component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,41 +2,52 @@ import { useContext } from "react";
 
 import Modal from "./ui/Modal";
 import CartContext from "../store/CartContext";
-import { FormatPrice } from "../util/format-price.util";
 import Button from "./ui/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
+import { PriceService } from "../util/price-service.util";
 
 export default function Cart() {
   const cartContext = useContext(CartContext);
   const userProgressContext = useContext(UserProgressContext);
 
-  const formatPrice = new FormatPrice();
+  const priceService = new PriceService();
 
-  const cartTotalPrice = cartContext.items.reduce(
-    (totalPrice, item) => totalPrice + item.quantity * item.price,
-    0
-  );
+  const cartTotalPrice = priceService.getCartTotalPrice(cartContext);
 
   function handleCloseCart() {
     userProgressContext.hideCart();
   }
 
+  function handleGoToCheckout() {
+    userProgressContext.showCheckout();
+  }
+
   return (
-    <Modal className="cart" open={userProgressContext.progress === "cart"}>
+    <Modal
+      className="cart"
+      open={userProgressContext.progress === "cart"}
+      onClose={userProgressContext.progress === "cart" ? handleCloseCart : null}
+    >
       <h2>Your Cart</h2>
       <ul>
         {cartContext.items.map((item) => (
-          <li key={item.id + item.name}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id + item.name}
+            item={item}
+            onIncrease={() => cartContext.addItem(item)}
+            onDecrease={() => cartContext.removeItem(item.id)}
+          />
         ))}
       </ul>
-      <p className="cart-total">{formatPrice.format(cartTotalPrice)}</p>
+      <p className="cart-total">{priceService.format(cartTotalPrice)}</p>
       <p className="modal-actions">
         <Button textOnly={true} onClick={handleCloseCart}>
           Close
         </Button>
-        <Button onClick={handleCloseCart}>Go To Checkout</Button>
+        {cartContext.items.length > 0 && (
+          <Button onClick={handleGoToCheckout}>Go To Checkout</Button>
+        )}
       </p>
     </Modal>
   );
