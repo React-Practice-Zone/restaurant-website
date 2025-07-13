32dcd4d3d968964ab8781910cafbf9f5e4d08dbe
Andrew Kononenko <wastardy.k@gmail.com>
2025-07-13 17:29:13 +0300

    feat(api): edit fetching meals from api logic

--- src/api/fetch-meals.js
+++ src/api/fetch-meals.js
@@ -1,12 +1,9 @@
 export async function getMeals() {
   try {
     const response = await fetch("http://localhost:5050/meals");
-
-    if (!response.ok) throw new Error("Failed to fetch meals");
-
     return await response.json();
   } catch (error) {
     console.error("Error fetching meals:", error);
-    throw error.message;
+    throw new Error(error.message || "Failed to fetch meals");
   }
 }
