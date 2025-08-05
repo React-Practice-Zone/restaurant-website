6ed284ca275e4d304eeb15cf637c2edd1ea9a708
Andrew Kononenko <wastardy.k@gmail.com>
2025-08-05 19:55:19 +0300

    feat(router): set up routing with RootLayout and define Home and Meals routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import Cart from "./components/Cart";
 import Checkout from "./components/Checkout";
 import Header from "./components/Header";
-import Meals from "./components/Meals";
+import Meals from "./pages/Meals";
+import Home from "./pages/Home";
 import { CartContextProvider } from "./store/CartContext";
 import { UserProgressContextProvider } from "./store/UserProgressContext";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./pages/Root/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/meals", element: <Meals /> },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <UserProgressContextProvider>
-      <CartContextProvider>
-        <Header />
-        <Meals />
-        <Cart />
-        <Checkout />
-      </CartContextProvider>
-    </UserProgressContextProvider>
+    <>
+      <UserProgressContextProvider>
+        <CartContextProvider>
+          <RouterProvider router={router} />
+        </CartContextProvider>
+      </UserProgressContextProvider>
+    </>
   );
 }
 
