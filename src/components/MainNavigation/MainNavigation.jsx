6cbd56ea69921bca4989809c88eaaa8742cb88a0
Andrew Kononenko <wastardy.k@gmail.com>
2025-08-05 20:02:35 +0300

    feat(navigation): add MainNavigation component with Header, Cart, and Checkout

---
+++ src/components/MainNavigation/MainNavigation.jsx
@@ -0,0 +1,13 @@
+import Cart from "../Cart";
+import Checkout from "../Checkout";
+import Header from "../Header";
+
+export default function MainNavigation() {
+  return (
+    <>
+      <Header />
+      <Cart />
+      <Checkout />
+    </>
+  );
+}
